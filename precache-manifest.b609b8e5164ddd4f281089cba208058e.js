c2646272ca1e7ef836f520eab6de8bdf1932ec63
Ryan Yang <ryanqyang@gmail.com>
2020-09-19 05:47:12 -0700

    Updates

---
+++ precache-manifest.b609b8e5164ddd4f281089cba208058e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8004f5da42a1aa0774b50ef9664201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def1b5f278b8b47a9868",
+    "url": "/static/css/2.85b71462.chunk.css"
+  },
+  {
+    "revision": "b7213896c899448117e0",
+    "url": "/static/css/main.07459081.chunk.css"
+  },
+  {
+    "revision": "def1b5f278b8b47a9868",
+    "url": "/static/js/2.cc8d0d1a.chunk.js"
+  },
+  {
+    "revision": "2415e6967177769184874497a3c934bc",
+    "url": "/static/js/2.cc8d0d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7213896c899448117e0",
+    "url": "/static/js/main.bc3a7eac.chunk.js"
+  },
+  {
+    "revision": "c609f2e78b7b6e1b44dd",
+    "url": "/static/js/runtime-main.aacd24d3.js"
+  },
+  {
+    "revision": "0824830a6b15877f5ac5c15171814436",
+    "url": "/static/media/logo-shadow.0824830a.svg"
+  }
+]);
